eaeeab2fc4fee71e0ba965a2fa06c9457c33b279
fiadot <fiadot@gmail.com>
2014-01-13 18:19:08 +0900

    close #6 bootstrap 3.0.3 적용

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
+
 from blog.models import Comment
 
 
 class CommentForm(ModelForm):
     class Meta:
-        model = Comment
+        model = Comment        
+        widgets = {            
+            'body' : Textarea(attrs={'cols': 80, 'rows' : 2}),
+        }
         exclude = ["post"]
